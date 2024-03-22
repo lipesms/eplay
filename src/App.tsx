7dbfdb34ffd925b926300dcbb6926a6f0931eca2
Felipe da Silva Martins <femartins2010@gmail.com>
2024-03-21 23:38:48 -0300

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import Banner from './components/Banner'
+
+import { GlobalCss } from './styles'
+
+function App() {
+  return (
+    <>
+      <GlobalCss />
+      <div className="container">
+        <Header />
+      </div>
+      <Banner />
+    </>
+  )
+}
+
+export default App
